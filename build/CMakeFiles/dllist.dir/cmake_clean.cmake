566b09362a5daa45de7066cd4090ebce92e2e563
Szymon <szymonkraj877@gmail.com>
2025-10-20 21:21:38 +0200

    Trzeci commit

---
+++ build/CMakeFiles/dllist.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dllist.dir/src/main.cpp.obj"
+  "CMakeFiles/dllist.dir/src/main.cpp.obj.d"
+  "dllist.exe"
+  "dllist.exe.manifest"
+  "dllist.lib"
+  "dllist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dllist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/dllist.dir/src/main.cpp.obj.d
@@ -0,0 +1,109 @@
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/iostream
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/yvals_core.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/vcruntime.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/sal.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/concurrencysal.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/vadefs.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xkeycheck.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/istream
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/ostream
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/ios
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xlocnum
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/cfloat
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/float.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/climits
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/limits.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/cmath
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/yvals.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/crtdbg.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/vcruntime_new_debug.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/vcruntime_new.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/crtdefs.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/use_ansi.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/cstdlib
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/math.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_math.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/stdlib.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_malloc.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_search.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/stddef.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wstdlib.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xtr1common
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/intrin0.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/intrin0.inl.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/cstdio
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/stdio.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wstdio.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_stdio_config.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/iterator
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/iosfwd
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/cstring
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/string.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_memory.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_memcpy_s.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/errno.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/vcruntime_string.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wstring.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/cwchar
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/wchar.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wconio.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wctype.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wdirect.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wio.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_share.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wprocess.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_wtime.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/sys/stat.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/sys/types.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xutility
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/__msvc_iter_core.hpp
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/utility
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/initializer_list
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/cstddef
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/type_traits
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/cstdint
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/stdint.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/streambuf
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xiosbase
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/share.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/system_error
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/__msvc_system_error_abi.hpp
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/cerrno
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/stdexcept
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/exception
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/malloc.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/vcruntime_exception.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/eh.h
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/corecrt_terminate.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xstring
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/__msvc_sanitizer_annotate_container.hpp
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/__msvc_string_view.hpp
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xmemory
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/limits
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/new
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xatomic.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xpolymorphic_allocator.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/tuple
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xcall_once.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xerrc.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/atomic
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xthreads.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/__msvc_threads_core.hpp
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xtimec.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/ctime
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/time.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xlocale
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/memory
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/typeinfo
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/vcruntime_typeinfo.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xfacet
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/xlocinfo
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/__msvc_xlocinfo_types.hpp
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/cctype
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/ctype.h
+D:/Microsoft Visual Studio/2022/Comunity/VC/Tools/MSVC/14.41.34120/include/clocale
+C:/Program Files (x86)/Windows Kits/10/include/10.0.22621.0/ucrt/locale.h
+C:/Users/simon/OneDrive/Pulpit/Programowanie_za/projekt_kod/include/DwukierunkowaLista.h
+C:/Users/simon/OneDrive/Pulpit/Programowanie_za/projekt_kod/include/NodeFactory.h
+C:/Users/simon/OneDrive/Pulpit/Programowanie_za/projekt_kod/include/Node.h
