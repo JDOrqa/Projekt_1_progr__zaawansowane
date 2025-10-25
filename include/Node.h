ce0dd54a03759b516f1774916a811891c409742c
Szymon <szymonkraj877@gmail.com>
2025-10-25 14:00:11 +0200

    Iterator

--- include/Node.h
+++ include/Node.h
@@ -2,15 +2,22 @@
 #define NODE_H
 
 #include <memory>
+/**
+ * @brief Prosty wêze³ listy dwukierunkowej (template).
+ * Doxygen-friendly komentarze.
+ */
 
-
+// Szablon struktury wêz³a dla listy dwukierunkowej
 template<typename T>
 struct Node {
-    T data;
-    Node<T>* prev;
-    Node<T>* next;
+    T data;        
+    Node<T>* prev;  
+    Node<T>* next;  
 
-    explicit Node(const T& value) : data(value), prev(nullptr), next(nullptr) {}
+   
+                                   //chyba od t¹d do pliku Node.cpp tak samo pewnie z factory
+    explicit Node(const T& value) : data(value), prev(nullptr), next(nullptr) {} // to do innegi pliku 
+   
 };
 
 #endif // NODE_H
--- include/NodeFactory.h
+++ include/NodeFactory.h
@@ -3,18 +3,24 @@
 
 #include "Node.h"
 
+/**
+ * @brief Prosta fabryka wêz³ów — wzorzec Factory.
+ * Pozwala odseparowaæ tworzenie wêz³ów (przydatne np. do testów, mocków itp.)
+ */
+
 
 template<typename T>
 class NodeFactory {
 public:
+   
     static Node<T>* createNode(const T& value) {
-        return new Node<T>(value);
+        return new Node<T>(value); 
     }
 
-    
+   
     static void destroyNode(Node<T>* node) {
-        delete node;
+        delete node; 
     }
 };
 
-#endif 
+#endif
